fe9dac7f3d7d21ed8979034d153024e677a01a36
Mwaki Magotswi <mmagotswi@bostonlogic.com>
2016-02-24 14:59:03 -0500

    deleting routes for dynamic form to match what we have in vendor/plugins

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,1 @@
-Rails.application.routes.draw do
-	resources :forms, :controller => 'dynamic_forms/forms' do
-		resources :form_submissions,
-          :controller => "dynamic_forms/form_submissions",
-          :only => [:index, :show, :new, :create]
-	end
-end
+
