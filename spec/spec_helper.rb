d47168ad1894d7bc41b0a757a780efb93fe6ed63
Mwaki Magotswi <mmagotswi@bostonlogic.com>
2016-02-24 17:50:13 -0500

    getting the empty spec suite to run

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,7 @@
 ENV["RAILS_ENV"] = "test"
 
 require File.expand_path("../dummy/config/environment.rb",  __FILE__)
+require 'minitest/autorun'
 require "rails/test_help"
 require "rspec/rails"
 
@@ -10,11 +11,6 @@ ActionMailer::Base.perform_deliveries = true
 ActionMailer::Base.default_url_options[:host] = "test.com"
 
 Rails.backtrace_cleaner.remove_silencers!
-
-# Configure capybara for integration testing
-require "capybara/rails"
-Capybara.default_driver   = :rack_test
-Capybara.default_selector = :css
 
 # Run any available migration
 ActiveRecord::Migrator.migrate File.expand_path("../dummy/db/migrate/", __FILE__)
