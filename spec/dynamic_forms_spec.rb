50b60168495d780bc6097c95957e5a98b0c00e8e
Mwaki Magotswi <mmagotswi@bostonlogic.com>
2016-02-25 13:38:16 -0500

    running a spec to insure that a config is loaded

--- spec/dynamic_forms_spec.rb
+++ spec/dynamic_forms_spec.rb
@@ -4,4 +4,8 @@ describe DynamicForms do
   it "should be valid" do
     DynamicForms.should be_a(Module)
   end
+
+  it "should have a configuration" do
+    DynamicForms.configuration.should_not be_nil
+  end
 end
