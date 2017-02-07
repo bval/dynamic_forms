b7369176d8cc161bc62346b3fdbf6e192d5891b7
Brandon Valentine <brandon@brandonvalentine.com>
2017-02-07 16:54:53 -0600

    Copy initializer into place in Rails generator
    Also remove some excess whitespace in the same file

--- lib/generators/dynamic_forms/dynamic_forms_generator.rb
+++ lib/generators/dynamic_forms/dynamic_forms_generator.rb
@@ -1,6 +1,6 @@
 class DynamicFormsGenerator < Rails::Generators::Base
   include Rails::Generators::Migration
-  
+
   def self.source_root
    @source_root ||= File.join(File.dirname(__FILE__), 'templates')
   end
@@ -12,8 +12,12 @@ class DynamicFormsGenerator < Rails::Generators::Base
   def create_migration_file
     migration_template 'migration.rb', 'db/migrate/create_dynamic_forms.rb'
   end
-  
+
   def copy_config_file
     template 'locales/dynamic_forms.yml', File.join('config', 'locales', 'dynamic_forms.yml')
+  end
+
+  def copy_initializer
+    template 'initializers/dynamic_forms.rb', File.join('config', 'initializers', 'dynamic_forms.rb')
   end
 end
