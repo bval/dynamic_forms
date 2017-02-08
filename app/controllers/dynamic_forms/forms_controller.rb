035cf31209d0d670bdd9348ef291a61ccd4f6d5b
Brandon Valentine <brandon@brandonvalentine.com>
2017-02-08 14:20:39 -0600

    Strong params for Rails 4+

--- app/controllers/dynamic_forms/forms_controller.rb
+++ app/controllers/dynamic_forms/forms_controller.rb
@@ -8,7 +8,7 @@ class DynamicForms::FormsController < ApplicationController
 
   # the Forms#show action actually renders FormSubmissions#new for displaying the form
   def show
-    @form = ::Form.find(params[:id])
+    @form = ::Form.find(form_params[:id])
     @form_submission = @form.form_submissions.build
     render :template => 'form_submissions/new'
   end
@@ -19,7 +19,7 @@ class DynamicForms::FormsController < ApplicationController
   end
 
   def edit
-    @form = ::Form.find(params[:id])
+    @form = ::Form.find(form_params[:id])
     render :template => "forms/edit"
   end
 
@@ -38,11 +38,11 @@ class DynamicForms::FormsController < ApplicationController
 
   def update
     # check to see if preview should be rendered rather than saving changes
-    preview_edit and return if params[:commit].to_s.downcase == 'preview'
+    preview_edit and return if form_params[:commit].to_s.downcase == 'preview'
 
-    @form = ::Form.find(params[:id])
+    @form = ::Form.find(form_params[:id])
 
-    if @form.update_attributes(params[:form])
+    if @form.update_attributes(form_params[:form])
       flash[:success] = translate(:form_updated, :scope => [:dynamic_forms, :controllers, :forms], :name => @form.name)
       redirect_to form_path(@form)
     else
@@ -51,7 +51,7 @@ class DynamicForms::FormsController < ApplicationController
   end
 
   def destroy
-    form = ::Form.find(params[:id])
+    form = ::Form.find(form_params[:id])
     form.destroy
     flash[:success] = translate(:form_deleted, :scope => [:dynamic_forms, :controllers, :forms], :name => form.name)
     redirect_to forms_path
@@ -60,7 +60,7 @@ class DynamicForms::FormsController < ApplicationController
   private
 
   def preview_new
-    @form = ::Form.new(params[:form])
+    @form = ::Form.new(form_params[:form])
     if @form.valid?
       @form_submission = @form.form_submissions.build
       @form_submission.form = @form # Believe it or not, this is necessary
@@ -69,13 +69,18 @@ class DynamicForms::FormsController < ApplicationController
   end
 
   def preview_edit
-    @form = ::Form.new(params[:form])
-    @form.id = params[:id]
+    @form = ::Form.new(form_params[:form])
+    @form.id = form_params[:id]
     if @form.valid?
       @form_submission = @form.form_submissions.build
       @form_submission.form = @form # Believe it or not, this is necessary
     end
     render :action => 'edit', :template => "forms/edit"
+  end
+
+  def form_params
+    #  => [:sti_type, :position, :label, :required, :min_length, :max_length, :_destroy]
+    params.require(:form).permit(:name, :instructions, :email, :form_fields_attributes, :commit)
   end
 
 end
